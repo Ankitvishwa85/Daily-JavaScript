c10114fc933dd862f8d80a329f5d5abf775a3aae
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-10 19:29:03 +0000

    Conversion Type

---
+++ 01_Basics/03_ConversonType.js
@@ -0,0 +1,59 @@
+/*
+let score = 33; //Number
+let scores = "33abc"; // String
+
+console.log(typeof score);//method 1..
+console.log(typeof(scores)); // method 2..
+
+let ValueInNumber = Number(scores);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber); //No A Number
+
+// null....
+
+let Age = null;
+
+let valueInNum = Number(Age);
+
+console.log(typeof Age);
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+//Boolean... 
+
+let Address = true;
+
+let valueInNumbers = Number(Address);
+
+console.log(typeof Address);
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers);
+
+*/
+
+let SomeNum = Ankit; // only NaN , no other string.
+console.log(SomeNum);
+
+
+let StringNum = String(SomeNum);
+
+console.log(StringNum);
+console.log(typeof StringNum);
+
+
+/*
+  String to number..
+
+ "33" => 33
+  "33abc" => NaN
+  true => 1, false => 0
+
+  string to boolean..
+  "" => false
+  "Ankit" => true
+
+  number to String..
+
+  33 => "33"
+  Nan => "NaN"
+*/
