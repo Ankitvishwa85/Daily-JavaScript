2e3ea3c353e2e2b0b49a8ce35fe2f99678074ba4
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-10 21:10:35 +0000

    Comparision Operator

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,32 @@
+// console.log(2 > 3); // greater than
+// console.log(2 < 3); // less than
+// console.log(2 == 3); // equal to
+// console.log(2 <= 3); // less than equal to
+// console.log(2 >= 3); // greater than equal to
+
+// compare value with DataType...
+
+// console.log("2" === 2); 
+// console.log(2 === 2);
+
+// Compare Null value...
+
+// console.log(null > 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //Relational operators(<,>,<=,>=) convert null to 0, but the equality operator(==) not.
+// console.log(null <= 0);
+
+
+//Compare Undefined Value...
+
+//undefined cannot be converted into a valid number for mathematical comparisons and turns into NaN (Not-a-Number), any greater/less than comparison made with it always results in false.
+
+console.log(undefined > 0); // NaN > 0
+console.log(undefined < 0); // NaN < 0
+console.log(undefined == 0);// NaN == 0
+console.log(undefined >= 0);
+
+
+console.log("2" == 2);  // comparision operator convert string into number.
+
