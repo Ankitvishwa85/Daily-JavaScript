456a9e898fde1f076f77d5299b0c500bbffed73e
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-10 21:10:03 +0000

    Operation

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,20 @@
+let value = 33;
+
+let NegValue = -value;
+
+console.log(NegValue);
+
+// console.log(3+3); //Add
+// console.log(3-3); //minus
+// console.log(3*3); //multiply
+// console.log(3**3); //power
+// console.log(3/3); //divide
+// console.log(3%3); //module
+
+let str1 = "Ankit"
+let str2 = " Vishwakarma"
+
+console.log(str1 + str2); //combine to string..
+
+
+
