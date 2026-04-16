d01d72d1a10bb601d0815940cc785d9975a283e2
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-16 05:23:17 +0000

    DataTypes Summary

---
+++ 01_Basics/dataTypes-summary.js
@@ -0,0 +1,19 @@
+/*  primitive DataTypes....
+
+7 types : string, Number, Null, Undefined, Symbol, boolean, BigInt
+
+Reference(Non-primitive DataTypes)..
+      Array, Objects, Functions 
+*/
+//Example....
+
+const heros = ["superman","Batman","Ironman"];
+let MyObj = {
+      name : "Ankit",
+      Age : 19,
+      email : "ankit85@gmail.com",
+}
+const MyFunction = function(){
+      console.log("Hello World");
+}
+console.log(typeof heros);
