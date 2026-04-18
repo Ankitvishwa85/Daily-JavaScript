ef8a5ce7c0059c7a948636ad3fe7c9a50a7132bd
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-18 20:11:37 +0000

    Number And Math

---
+++ 01_Basics/07_Number-math.js
@@ -0,0 +1,27 @@
+const score = 400
+              // primitive
+console.log(score);
+
+const Balance = new Number(100) // Object
+console.log(Balance);
+console.log(Balance.toString().length);
+console.log(Balance.toFixed(2));  // Add Decimal with two zero...
+
+
+const otherNumber = 2334.6545;
+console.log(otherNumber.toPrecision(4));  // give precise value of 2334.6545. (2335)ans
+
+
+const hundred = 100000
+console.log(hundred.toLocaleString('en-IN')); // print indian format...
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// =======================MATHS==============================
+
+console.log(Math)
+console.log(typeof Math) // object
+
+console.log(Math.abs(-4));  //positive banane ke liye..
+console.log(Math.SQRT2); // Squre root nikalne ke liye.
+ 
