9b5c31cda30253d6f504369f4a0b930ebb6800a2
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-21 19:21:46 +0000

    function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,4 +1,4 @@
-function sayMyName(){
+function sayMyName() {
     console.log('A');
     console.log('N');
     console.log('K');
@@ -15,18 +15,71 @@ function AddtwoNumber( number1, number2){
 AddtwoNumber(12, 23);
 */
 
-function AddtwoNumber( number1, number2){
-   let result = number1 + number2;
-   return result; // After Return no any one lines Are execute.
-//    return number1 + number2;
+function AddtwoNumber(number1, number2) {
+    let result = number1 + number2;
+    return result; // After Return no any one lines Are execute.
+    //    return number1 + number2;
 }
 const result = AddtwoNumber(23, 65);
-console.log("Result is: ", result);
+// console.log("Result is: ", result); 
 
 
-function loginUserMessage(username){
+function loginUserMessage(username) {
     return `${username} just logged in.`
 }
-console.log(loginUserMessage("Ankit"));
+// console.log(loginUserMessage("Ankit"));
 
 
+
+
+
+function calculateCartPrice(...num1) {   // here (...) is rest operator...
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 3000, 4000, 50000));
+
+function calculateCartPrice2(value1, value2, ...num1) {
+    return num1
+}
+// console.log(calculateCartPrice2(200, 3000, 4000, 50000)); // here output is [4000, 50000] bcoz value1 stored 200 and value2 3000;
+
+
+const user = {
+    username: 'Ankit',
+    salary: 29999
+}
+function handleObject(anyObject) {
+    console.log(`My name is ${anyObject.username}  and my salary is ${anyObject.salary}.`)
+}
+// handleObject(user)
+handleObject({
+    username: "Amit",
+    salary: 31000
+})
+
+
+const us = {
+    city: 'california',
+    app: 'Amazon'
+}
+function check(App){
+ console.log(`City name is ${App.city} and App is ${App.app}.`)
+ return App;
+}
+ const service = check(us);
+ console.log(service);
+ 
+
+
+
+ //Array in Function..
+
+ //const myArray = [200, 300, 400, 500]
+
+ function returnSecondValue(getArray){
+    return getArray[1]
+ }
+ //console.log(returnSecondValue(myArray));
+ console.log(returnSecondValue([100, 30, 40, 700]));
+ 
