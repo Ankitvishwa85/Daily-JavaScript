1c0bb5a4e85566c313b91ce63e78bddf3e4fab82
Ankit Vishwak. <215135935+Ankitvishwa85@users.noreply.github.com>
2026-04-26 01:10:56 +0000

    Switch Statement

---
+++ switch.js
@@ -0,0 +1,32 @@
+// switch(key){
+//     case 1:
+
+//     break;
+//     case 2:
+
+//     break;
+
+//     default:
+//      break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("Febraury");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+
+    default:
+        console.log("Sorry");
+        
+        break;
+}
